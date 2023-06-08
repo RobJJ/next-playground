1b07d86ad47f1fe67abfb4667d94ad868899a4f0
RobJ <robjjohnsenwork@gmail.com>
2023-06-08 17:28:23 +0700

    added react query and setup fetch of users

--- src/app/components/UserCard.jsx
+++ src/app/components/UserCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const UserCard = () => {
+const UserCard = ({ user }) => {
   return (
     <div className="bg-slate-300 w-64 h-32 rounded flex flex-col justify-around pl-2">
       <h4>Name:</h4>
--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,6 +1,27 @@
+"use client"; // have to set this because we are using useEffect hook
+
+import { useQuery } from "@tanstack/react-query";
 import UserCard from "../components/UserCard";
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const UsersPage = () => {
+  const {
+    data: usersData = [],
+    isLoading,
+    isError,
+  } = useQuery(["userData"], async () => {
+    await sleep(2000);
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    return await res.json();
+  });
+
+  console.log("[DATA] : ", usersData);
+  console.log("[LOADING] : ", isLoading);
+  console.log("[ERROR] : ", isError);
+
   return (
     <div className="w-full h-full p-1 flex flex-col gap-2">
       <h1 className="w-full bg-blue-100 text-2xl font-bold text-center underline">
@@ -8,16 +29,9 @@ const UsersPage = () => {
         fetched from JSON plaholder
       </h1>
       <section className="w-full h-full bg-blue-100 flex flex-wrap p-5 gap-5 items-center justify-center">
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
+        {usersData.map((user) => {
+          return <UserCard key={user.id} user={user} />;
+        })}
       </section>
     </div>
   );
