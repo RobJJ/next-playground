4b3405d1a98a6a77f2925e91da77b3e94615833e
RobJ <robjjohnsenwork@gmail.com>
2023-06-15 10:00:38 +0700

    removed mongo api route for now

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,30 +1,30 @@
-import clientPromise from "../lib/mongodb";
+// import clientPromise from "../lib/mongodb";
 
-export async function GET() {
-  try {
-    const client = await clientPromise;
-    const db = client.db("district-data");
+// export async function GET() {
+//   try {
+//     const client = await clientPromise;
+//     const db = client.db("district-data");
 
-    const collection = db.collection("districts");
-    const documents = await collection.find({
-      DISTRICT_ID: "UZB.1.10_1",
-    });
-    // const documents = await collection.find({ id: "UZB.1.10_1" }).toArray();
-    return res.status(200).json(documents);
+//     const collection = db.collection("districts");
+//     const documents = await collection.find({
+//       DISTRICT_ID: "UZB.1.10_1",
+//     });
+//     // const documents = await collection.find({ id: "UZB.1.10_1" }).toArray();
+//     return res.status(200).json(documents);
 
-    // const movies = await db
-    //   .collection("movies")
-    //   .find({})
-    //   .sort({ metacritic: -1 })
-    //   .limit(10)
-    //   .toArray();
+//     // const movies = await db
+//     //   .collection("movies")
+//     //   .find({})
+//     //   .sort({ metacritic: -1 })
+//     //   .limit(10)
+//     //   .toArray();
 
-    // res.json(movies);
-  } catch (error) {
-    return res
-      .status(500)
-      .send({ message: "An error occurred.", error: error.toString() });
+//     // res.json(movies);
+//   } catch (error) {
+//     return res
+//       .status(500)
+//       .send({ message: "An error occurred.", error: error.toString() });
 
-    // console.error(e);
-  }
-}
+//     // console.error(e);
+//   }
+// }
